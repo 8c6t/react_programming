0bb74370c8c0476c20a1987401f1df36bc65f125
8c6t <nkw8620@gmail.com>
2019-07-18 02:00:48 +0900

    1.4.4 css-in-js로 작성하기
    패키지 설치 관련 문제 해결

--- ch01/06_css_test/src/App.js
+++ ch01/06_css_test/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Button3 from './Button3';
-import Box3 from './Box3';
+import Box5 from './Box5';
 
 function App() {
   return (
     <>
       <Button3 size="big" />
       <Button3 size="small" />
-      <Box3 size="big" />
-      <Box3 size="small" />
+      <Box5 size="big" />
+      <Box5 size="small" />
     </>
   );
 }
