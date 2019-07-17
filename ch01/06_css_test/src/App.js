814b966cfc93a0961623b59c1b3bdca3194d5f0e
8c6t <nkw8620@gmail.com>
2019-07-18 01:26:11 +0900

    1.4.3 Sass로 작성하기

--- ch01/06_css_test/src/App.js
+++ ch01/06_css_test/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Button2 from './Button2';
-import Box2 from './Box2';
+import Button3 from './Button3';
+import Box3 from './Box3';
 
 function App() {
   return (
     <>
-      <Button2 size="big" />
-      <Button2 size="small" />
-      <Box2 size="big" />
-      <Box2 size="small" />
+      <Button3 size="big" />
+      <Button3 size="small" />
+      <Box3 size="big" />
+      <Box3 size="small" />
     </>
   );
 }
