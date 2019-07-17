0cb40723893dea2d8c2c650c64787e8e8b7abdce
8c6t <nkw8620@gmail.com>
2019-07-17 21:13:19 +0900

    1.4.1 일반적인 CSS 파일로 작성하기

---
+++ ch01/06_css_test/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from './Button';
+import Box from './Box';
+
+function App() {
+  return (
+    <>
+      <Button size="big" />
+      <Button size="small" />
+      <Box size="big" />
+      <Box size="small" />
+    </>
+  );
+}
+
+export default App;
