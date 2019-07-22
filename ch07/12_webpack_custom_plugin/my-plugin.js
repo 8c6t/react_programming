3a8067686a4b8deebbfb03fbe35b7550ab7392fc
8c6t <nkw8620@gmail.com>
2019-07-22 16:00:12 +0900

    7.4.3 로더 제작하기

---
+++ ch07/12_webpack_custom_plugin/my-plugin.js
@@ -0,0 +1,30 @@
+class MyPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    compiler.hooks.done.tap('MyPlugin', () => {
+      console.log('bundling complited');
+    });
+    compiler.hooks.emit.tap('MyPlugin', compilation => {
+      let result = '';
+      for (const filename in compilation.assets) {
+        if (this.options.showSize) {
+          const size = compilation.assets[filename].size();
+          result += `${filename}(${size})\n`;
+        }
+      }
+      compilation.assets['fileList.txt'] = {
+        source: function() {
+          return result;
+        },
+        size: function() {
+          return result.length;
+        }
+      }
+    });
+  }
+}
+
+module.exports = MyPlugin;
