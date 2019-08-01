657880dc358af9441ae466eb3fae589ae4d0ada5
8c6t <nkw8620@gmail.com>
2019-08-02 08:58:59 +0900

    8.3.3 서버에서 생성된 데이터를 전달하기

---
+++ ch08/03_test_next/pages/page2.js
@@ -0,0 +1,21 @@
+import { callApi } from '../src/api';
+
+const Page2 = ({ text, data, userAgent }) => {
+  return (
+    <div>
+      <p>this is home page2</p>
+      <p>{`text: ${text}`}</p>
+      <p>{`data is ${data}`}</p>
+      <p>{`userAgent: ${userAgent}`}</p>
+    </div>
+  );
+}
+
+Page2.getInitialProps = async ({ query, req }) => {
+  const text = query.text || 'none';
+  const data = await callApi();
+  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+  return { text, data, userAgent };
+}
+
+export default Page2;
