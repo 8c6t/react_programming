42d4afc93c6c2a124bf81cb91b6452b4eced8d43
8c6t <nkw8620@gmail.com>
2019-08-02 11:01:29 +0900

    8.4.1 페이지 공통 기능 구현하기

---
+++ ch08/04_next_advanced/pages/page1.js
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import Icon from '../static/icon.png';
+
+const Page1 = () => {
+  return (
+    <div>
+      <p>This is home page</p>
+      <Head>
+        <title>page1</title>
+        <img src={Icon} />
+        <Head>
+          <meta name="description" content="hello world" />
+        </Head>
+      </Head>
+      <style jsx>{`
+        p {
+          color: blue;
+          font-size: 18pt;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export default Page1;
