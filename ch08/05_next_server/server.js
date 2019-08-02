955b93ac8e94ac5a5c40059c9f27402f06da07e2
8c6t <nkw8620@gmail.com>
2019-08-02 12:26:46 +0900

    8.4.4 서버사이드 렌더링 캐싱하기

--- ch08/05_next_server/server.js
+++ ch08/05_next_server/server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const next = require('next');
+const url = require('url');
+const lruCache = require('lru-cache');
+
+const ssrCache = new lruCache({
+  max: 100,
+  maxAge: 1000 * 60,
+});
 
 const port = 3000;
 const dev = process.env.NODE_ENV !== 'production';
@@ -9,8 +16,8 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  server.get('/page/:id', (req, res) => {
-    res.redirect(`/page${req.params.id}`);
+  server.get(/^\/page[1-9]/, (req, res) => {
+    return renderAndCache(req, res);
   });
 
   server.get('*', (req, res) => {
@@ -22,3 +29,25 @@ app.prepare().then(() => {
     console.log(`> Ready on http://localhost:${port}`);
   });
 });
+
+const renderAndCache = async (req, res) => {
+  const parsedUrl = url.parse(req.url, true);
+  const cacheKey = parsedUrl.path;
+
+  if (ssrCache.has(cacheKey)) {
+    console.log('캐시 사용');
+    res.send(ssrCache.get(cacheKey));
+    return;
+  }
+
+  try {
+    const { query, pathname } = parsedUrl;
+    const html = await app.renderToHTML(req, res, pathname, query);
+    if (res.statusCode === 200) {
+      ssrCache.set(cacheKey, html);
+    }
+    res.send(html);
+  } catch (error) {
+    app.renderError(err, req, res, pathame, query);
+  }
+}
