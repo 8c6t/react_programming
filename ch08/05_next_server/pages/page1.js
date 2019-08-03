4b0191721b8fa757319e42c3ceebd09a4d34e8ba
8c6t <nkw8620@gmail.com>
2019-08-03 21:29:59 +0900

    8.4.6 styled-components 사용하기

--- ch08/05_next_server/pages/page1.js
+++ ch08/05_next_server/pages/page1.js
@@ -1,12 +1,19 @@
 import Head from 'next/head';
+import styled from 'styled-components';
+
 import Icon from '../static/icon.png';
 import { add } from '../src/util';
+
+const MyP = styled.div`
+  color: blue;
+  font-size: 18pt;
+`;
 
 const Page1 = () => {
   return (
     <div>
-      <p>{`10 + 20 = ${add(10, 20)}`}</p>
-      <p>This is home page</p>
+      <MyP>{`10 + 20 = ${add(10, 20)}`}</MyP>
+      <MyP>This is home page</MyP>
       <Head>
         <title>page1</title>
         <img src={Icon} />
@@ -14,12 +21,6 @@ const Page1 = () => {
           <meta name="description" content="hello world" />
         </Head>
       </Head>
-      <style jsx>{`
-        p {
-          color: blue;
-          font-size: 18pt;
-        }
-      `}</style>
     </div>
   );
 }
